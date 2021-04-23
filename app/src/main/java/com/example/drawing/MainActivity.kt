87d8a447cd9ff581a3e403f39e02c05716af3403
Kaivalya Naik <kaivalya.naik@joshsoftware.com>
2021-04-23 10:48:14 +0530

    8 petal flower

--- app/src/main/java/com/example/drawing/MainActivity.kt
+++ app/src/main/java/com/example/drawing/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.drawing
 
+import android.content.res.ColorStateList
 import android.graphics.*
 import android.os.Bundle
 import android.transition.Fade
@@ -7,6 +8,7 @@ import android.transition.TransitionManager
 import android.view.animation.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.google.android.material.transition.MaterialFadeThrough
 
 
@@ -20,8 +22,8 @@ class MainActivity : AppCompatActivity() {
 
         fadeIn.duration = 2000
 
-        val rotateAnimation =RotateAnimation(270f,360f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
-        rotateAnimation.duration=1200
+        val rotateAnimation =RotateAnimation(315f,360f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
+        rotateAnimation.duration=1000
         rotateAnimation.interpolator=LinearInterpolator()
 
         val animation = AnimationSet(false) //change to false
@@ -32,6 +34,14 @@ class MainActivity : AppCompatActivity() {
 
         val view:ConstraintLayout=findViewById(R.id.flower)
 
+        val svgPetal:SvgPetal =findViewById(R.id.svgPetal)
+
+        svgPetal.backgroundTintList= ColorStateList.valueOf(ContextCompat.getColor(this,R.color.red))
+
+        val svgPetal315:SvgPetal =findViewById(R.id.petal315)
+
+        svgPetal315.text="200"
+        svgPetal315.backgroundTintList=ColorStateList.valueOf(ContextCompat.getColor(this,R.color.orange))
 
         view.animation=animation
 
